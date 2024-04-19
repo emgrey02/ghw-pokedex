a4ee8f8347ad247cd740a6de04f45520781db2cd
emgrey02 <emgrey02@gmail.com>
2024-04-19 14:46:31 -0400

    Add transitions and media queries

--- src/app/PokeInfo.jsx
+++ src/app/PokeInfo.jsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import { useState} from 'react';
 
-export default function PokeInfo({ currentPoke }) {
+export default function PokeInfo({ currentPoke, tr, mo }) {
     const [audioIsPlaying, setAudioIsPlaying] = useState(false);
 
     const playAudio = (e) => {
@@ -14,7 +14,7 @@ export default function PokeInfo({ currentPoke }) {
     }
     
     return (
-        <div className='place-self-center z-20 text-zinc-700 dark:text-gray-400 w-fit h-min flex flex-col gap-4 p-8 bg-zinc-400/50 dark:bg-zinc-600/50 outline outline-slate-600 shadow-xl shadow-slate-900/30 rounded-md'>
+        <div id='card' className={`place-self-center z-20 text-zinc-700 dark:text-gray-400 w-fit h-min flex flex-col gap-4 p-8 bg-zinc-400/50 dark:bg-zinc-600/50 outline outline-slate-600 shadow-xl shadow-slate-900/30 rounded-md ${tr && 'in'} ${mo && 'visible'}`}>
             <div className='grid grid-cols-2 h-min w-full gap-6 pb-10'>
                 <div className='flex flex-col ring-1 ring-indigo-900 dark:ring-indigo-300 p-4 self-start'>
                     <button title='Hear My Cry!' className='hover:bg-gray-100/10 rounded-full transition-all' onClick={playAudio}>
@@ -28,17 +28,17 @@ export default function PokeInfo({ currentPoke }) {
                         />
                     </button>
                     <div className='flex flex-col items-center'>
-                        <span className='text-sm'>Click me to hear my cry!</span>
-                        <span className='text-sm'>(audio warning)</span>
+                        <span className='text-xs md:text-sm text-center transition-all'>Click me to hear my cry!</span>
+                        <span className='text-xs md:text-sm'>(audio warning)</span>
                     </div>
                 </div>
                 <div className='flex flex-col gap-8'>
-                    <h2 className='text-zinc-900 dark:text-zinc-300 text-3xl font-bold'>{currentPoke.species.name.charAt(0).toUpperCase() + currentPoke.species.name.slice(1)}</h2>
+                    <h2 className='text-zinc-900 dark:text-zinc-300 text-2xl md:text-3xl font-bold transition-all'>{currentPoke.species.name.charAt(0).toUpperCase() + currentPoke.species.name.slice(1)}</h2>
                     <div className='flex flex-col gap-2'>
                         {currentPoke.types.map((type, index) =>
                         (
-                        <div className='w-min flex flex-col items-center rounded-full border-2 border-indigo-900 dark:border-indigo-300 ' key={index}>
-                                <p className='self-start px-2 py-px text-indigo-900 dark:text-indigo-300 font-semibold'>{type.type.name}</p>
+                        <div className='w-min flex flex-col items-center rounded-full border-2 border-indigo-900 dark:border-indigo-300' key={index}>
+                                <p className='self-start px-2 py-px text-indigo-900 dark:text-indigo-300 text-sm md:text-base transition-all font-semibold'>{type.type.name}</p>
                         </div>
                         ))
                         }
@@ -47,16 +47,16 @@ export default function PokeInfo({ currentPoke }) {
             </div>
             <div className='grid grid-cols-3 items-center'>
                 <div className='flex flex-col items-center justify-center'>
-                    <p className='text-3xl text-indigo-900 dark:text-indigo-300 font-semibold'>{currentPoke.weight}</p>
-                    <h3 className='text-sm'>Weight</h3>
+                    <p className='text-2xl md:text-3xl text-indigo-900 dark:text-indigo-300 font-semibold transition-all'>{currentPoke.weight}</p>
+                    <h3 className='text-xs md:text-sm transition-all'>Weight</h3>
                 </div>
                 <div className='flex flex-col items-center justify-center'>
-                    <p className='text-3xl text-indigo-900 dark:text-indigo-300 font-semibold'>{currentPoke.height}</p>
-                    <h3 className='text-sm'>Height</h3>
+                    <p className='text-2xl md:text-3xl text-indigo-900 dark:text-indigo-300 font-semibold transition-all'>{currentPoke.height}</p>
+                    <h3 className='text-xs md:text-sm transition-all'>Height</h3>
                 </div>
                 <div className='flex flex-col items-center justify-center'>
-                    <p className='text-3xl text-indigo-900 dark:text-indigo-300 font-semibold'>{currentPoke.base_experience}</p>
-                    <h3 className='text-sm'>Base XP</h3>
+                    <p className='text-2xl md:text-3xl text-indigo-900 dark:text-indigo-300 font-semibold transition-all'>{currentPoke.base_experience}</p>
+                    <h3 className='text-xs md:text-sm transition-all'>Base XP</h3>
                 </div>
             </div>
             <div className='col-span-3 h-px bg-slate-600 dark:bg-gray-500 rounded-lg'></div>
@@ -66,8 +66,8 @@ export default function PokeInfo({ currentPoke }) {
                 {currentPoke.stats.map((stat, index) =>
                 (
                     <div className='flex flex-col items-center' key={index}>
-                        <p className='text-3xl text-indigo-900 dark:text-indigo-300 font-semibold'>{stat.base_stat}</p>
-                        <p className='text-sm'>{stat.stat.name}</p>
+                        <p className='text-2xl md:text-3xl text-indigo-900 dark:text-indigo-300 font-semibold transition-all'>{stat.base_stat}</p>
+                        <p className='text-xs md:text-sm text-center transition-all'>{stat.stat.name}</p>
                     </div>
                 ))
                 }
