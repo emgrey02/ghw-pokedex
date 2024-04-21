23854433bde7981a13b38c0f4742334d388de9cc
emgrey02 <emgrey02@gmail.com>
2024-04-21 19:41:12 -0400

    Clean up styles and fix focus states

--- src/app/PokemonList.jsx
+++ src/app/PokemonList.jsx
@@ -8,7 +8,7 @@ export default function PokemonList({ pokemonList, showPokeInfo }) {
                 pokemonList.map((poke, index) =>
                 (
                 <li className='flex flex-col justify-between items-center' key={poke.order}>
-                    <button className='h-full px-3 text-gray-700 dark:text-gray-400  hover:dark:text-gray-300 hover:text-gray-950 hover:font-semibold py-3  hover:ring-slate-500 hover:dark:ring-slate-950/60 rounded-lg hover:shadow-lg hover:shadow-slate-500/60 hover:dark:shadow-slate-900/80 hover:bg-sky-300/40 hover:dark:bg-indigo-900/20 focus:outline-0 focus:ring-2 focus:ring-slate-600 transition-all' onClick={showPokeInfo} data-order={index}>
+                    <button className='h-full px-3 text-gray-700 dark:text-gray-400  hover:dark:text-gray-300 hover:text-gray-950 hover:font-semibold py-3 hover:ring-slate-500 hover:dark:ring-slate-950/60 rounded-lg hover:shadow-lg hover:shadow-slate-500/60 hover:dark:shadow-slate-900/80 hover:bg-sky-300/40 hover:dark:bg-indigo-900/20 focus:outline-0 focus:ring-2 ring-slate-600 dark:ring-slate-300 transition-all' onClick={showPokeInfo} data-order={index}>
                         <Image
                             src={poke.sprites.front_default}        
                             width={100}
